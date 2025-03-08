0d375c5da99a193bd580cfa13d0784b45c1c8e6a
asadbek <asadbekqulboyev@gmail.com>
2025-03-08 06:09:09 +0500

    Finish desctop

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // $('.mobile_menu_btn').on('click',function(e){
+    //     e.preventDefault();
+    //     $('.mobile_menu').addClass('active');
+    //     $('body').addClass('active');
+    // })
+    // $('.close_menu').on('click',function(e){
+    //     e.preventDefault();
+    //     $('.mobile_menu').removeClass('active');
+    //     $('body').removeClass('active');
+    // })
+
+    // $(document).ready(function () {
+    //     $(".select_btn").click(function (event) {
+    //         event.preventDefault(); // Standart o'tishni bloklash
+    //         $("#exampleModal").modal("show"); // Modalni ochish
+    //     });
+    // });
+})
